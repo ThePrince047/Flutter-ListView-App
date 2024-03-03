f22266921b52b9a93915d303b58f080cf49b6d3f
Anonymousagent47 <princesolanki4747@gmail.com>
2024-03-03 22:25:49 +0530

    app 50% done

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,95 @@
 import 'package:flutter/material.dart';
 
+class Food {
+  final String name;
+  final String urlfood;
+
+  const Food({
+    required this.name,
+    required this.urlfood,
+  });
+}
+
+
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  final List<Food> food = [
+    Food(
+      name: "Pizza",
+      urlfood:
+      "https://cdn3d.iconscout.com/3d/premium/thumb/pizza-slice-4706088-4020120.png?f=webp",
+    ),
+    Food(
+      name: "HotDog",
+      urlfood:
+      "https://cdn3d.iconscout.com/3d/premium/thumb/hotdog-4706055-4020123.png?f=webp",
+    ),
+    Food(
+      name: "Taco",
+      urlfood:
+      "https://cdn3d.iconscout.com/3d/premium/thumb/taco-4706092-4020117.png?f=webp",
+    ),
+    Food(
+      name: "Donut",
+      urlfood:
+      "https://cdn3d.iconscout.com/3d/premium/thumb/donuts-4706053-4020125.png?f=png",
+    ),
+    Food(
+      name: "French Fries",
+      urlfood:
+      "https://cdn3d.iconscout.com/3d/premium/thumb/french-fries-4706054-4020124.png?f=png",
+    ),
+    Food(
+      name: "Ice Cream",
+      urlfood:
+      "https://cdn3d.iconscout.com/3d/premium/thumb/ice-cream-cone-4706056-4020122.png?f=webp",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Food App",
+            style: TextStyle(
+              fontSize: 30,
+              fontStyle: FontStyle.normal,
+              color: Colors.white,
+              fontFamily: 'jellee',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          backgroundColor: Colors.cyan,
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: food.length,
+          itemBuilder: (context, index) => Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 28,
+                backgroundImage: NetworkImage(food[index].urlfood),
+              ),
+              title: Text(
+                food[index].name,
+                style: TextStyle(fontFamily: 'jellee'),
+              ),
+              trailing: const Icon(Icons.arrow_forward),
+              //onTap: (){
+              //  Navigator.of(context).push(MaterialPageRoute(builder: (context) => FoodPage(food:food),
+                //));
+             // },
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
